db6d5d56b0adc540f811c2ffda101227fcb96161
Kaio Antônio <kaioaar@gmail.com>
2023-05-04 23:08:32 -0300

    feat: adicionando dependências

---
+++ src/main/java/com/singular/coffedelivery/CoffedeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.singular.coffedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffedeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffedeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singular/coffedelivery/CoffedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singular.coffedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
