cf0261b09a7a9c5d8870358280bedd775119bfe5
Thassio Vagner Dantas dos Santos <thassio.santos@dbccompany.com.br>
2023-05-05 12:40:13 -0300

    feat(usuario): adiciona entidade de usuário

---
+++ src/main/java/com/singular/coffedelivery/entity/UsuarioEntity.java
@@ -0,0 +1,31 @@
+package com.singular.coffedelivery.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "usuario")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+}
